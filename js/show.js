b33a03d74c5651aa5487398e8c05a48e1fa0e815
Hekiera Mareroa <h3ckyeah@gmail.com>
2015-08-19 11:29:21 +1200

    add hover effect to the image and will fix the sizing of the modal image so that it resizes to the window/device size

--- js/show.js
+++ js/show.js
@@ -4,6 +4,7 @@ $( function() {
   $("img#popup").click( function( event ) {
     event.stopPropagation();
     // write classes and css to tags
+    $("body, html").addClass('overflow');
     $(".modal").fadeIn( "slow" );
     $("#close").fadeIn( "slow" );
   });
@@ -14,7 +15,24 @@ $( function() {
   // make the background close the modal on click
   $(".modal").click( function() {
     // change body css and remove classes
+    $("body, html").removeClass('overflow');
     $(".modal").fadeOut( "slow" );
     $("#close").fadeOut( "slow" );
+  });
+});
+
+$( function() {
+  // Click to show modal
+  $("img#popup").mouseover( function( event ) {
+    event.stopPropagation();
+    // write classes and css to tags
+    $("article").addClass('light');
+    $(".grizz-container").addClass('dark');
+  });
+  // make the background close the modal on click
+  $("img#popup").mouseout( function() {
+    // change body css and remove classes
+    $("article").removeClass('light');
+    $(".grizz-container").removeClass('dark');
   });
 });
